9f093ba8aa59e6dd49bb4551c387f73f7908f485
Randall Leeds <randall@bleeds.info>
2015-10-10 19:59:18 -0700

    Rewrite without dependencies
    Use a more straightforward and symmetrical algorithm by abstracting the
    traversals that find leaf nodes within the range over the range offsets
    and a step traversal step function.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
-var indexOf = require('index-of');
-var debug = require('debug')('range-normalize');
-
-// map to an Object for faster lookup times
-var voidElements = require('void-elements').reduce(function (obj, name) {
-  obj[name.toUpperCase()] = true;
-  return obj;
-}, {});
-
 /**
  * Module exports.
  */
@@ -26,31 +12,16 @@ module.exports = normalize;
  *
  * Pseudo-logic is as follows:
  *
- * For the "start":
- *
- *  - Is start container already a text node?
- *    - Is start offset at the end of the text node?
- *      - Traverse "up" the start container until a `nextSibling` is found
- *      - Set new offset = 0
- *      - Go back to start, repeat
- *    - Else
- *      - Done! start container and offset are normalized
- *  - Else
- *    - Traverse "down" the start container until either a text node or void element is found
- *    - Set new start offset = 0
- *    - Is new start container a text node?
- *      - Go back to start, repeat
- *    - Else, is new start container a void element?
- *      - Set start container's `parentNode` as new start container
- *      - Set new start offset = indexOf(new start container's `childNodes`, old start container "void element")
- *    - Else (assume start container is any other HTML element which may or may not contain children)
- *      - ???
- *
- * For the "end":
- *
- *  - Is end container already a text node?
- *    - Is end offset at the beginning of the text node?
- *      - Traverse "up" the end container until a `previousSibling` is found
+ * - Expand the boundaries if they fall between siblings.
+ * - Narrow the boundaries until they point at leaf nodes.
+ * - Is the start container excluded by its offset?
+ *   - Move it to the next leaf Node, but not past the end container.
+ *   - Is the start container a leaf Node but not a TextNode?
+ *     - Set the start boundary to be before the Node.
+ * - Is the end container excluded by its offset?
+ *   - Move it to the previous leaf Node, but not past the start container.
+ *   - Is the end container a leaf Node but not a TextNode?
+ *     - Set the end boundary to be after the Node.
  *
  * @param {Range} range - DOM Range instance to "normalize"
  * @return {Range} returns `range`, after being "normalized"
@@ -61,164 +32,103 @@ function normalize (range) {
   var so = range.startOffset;
   var ec = range.endContainer;
   var eo = range.endOffset;
-  var collapsed = range.collapsed;
-  var end;
 
-  if (!collapsed && sc.nodeType === 3 && so === sc.nodeValue.length) {
-    debug('start is at end of TextNode, need to move to `nextSibling`');
-
-    while (sc && !sc.nextSibling) {
-      sc = sc.parentNode;
-    }
-
-    if (sc) {
-      sc = sc.nextSibling;
-      so = 0;
-    } else {
-      debug('could not find TextNode within %o, resetting `sc`', range.startContainer);
-      sc = range.startContainer;
-    }
+  // Move the start container to the last leaf before any sibling boundary.
+  if (sc.childNodes.length && so > 0) {
+    sc = lastLeaf(sc.childNodes[so-1]);
+    so = sc.length || 0;
   }
 
-
-  if (sc.nodeType === 1) {
-    if (voidElements[sc.nodeName]) {
-      debug('start is a "void element", need to use parent node', sc);
-      var v = sc;
-      sc = v.parentNode;
-      so = indexOf(sc.childNodes, v);
-    } else {
-      debug('start is an Element, need to find deepest child node at offset %o', so);
-
-      if (so >= sc.childNodes.length) {
-        end = true;
-        sc = sc.childNodes[sc.childNodes.length - 1];
-      } else {
-        end = false;
-        sc = sc.childNodes[so];
-      }
-
-      var c;
-      while (sc) {
-        if (sc.nodeType === 3) break;
-        if (end) {
-          c = sc.lastChild;
-        } else {
-          c = sc.firstChild;
-        }
-        if (c && voidElements[c.nodeName]) break;
-        sc = c;
-      }
-
-      if (sc) {
-        if (sc.nodeType === 1) {
-          // a "void element"'s parent
-          so = end ? sc.childNodes.length : 0;
-        } else {
-          // text node
-          so = end ? sc.nodeValue.length : 0;
-        }
-      } else {
-        debug('could not find TextNode within %o, resetting `sc`', range.startContainer);
-        sc = range.startContainer;
-        so = range.startOffset;
-      }
-    }
+  // Move the end container to the first leaf after any sibling boundary.
+  if (eo < ec.childNodes.length) {
+    ec = firstLeaf(ec.childNodes[eo]);
+    eo = 0;
   }
 
-  if (collapsed) {
-    // if we're dealing with a collapsed Range, then update the end
-    // points to match our normalized start container and offset
-    ec = sc;
-    eo = so;
-  } else {
-    while (true) {
+  // Move each container inward until it reaches a leaf Node.
+  var start = firstLeaf(sc);
+  var end = lastLeaf(ec);
 
-      if (eo === 0) {
-        debug('end is at start of Node, need to move to `previousSibling`');
-
-        while (ec && !ec.previousSibling) {
-          ec = ec.parentNode;
-        }
-
-        if (ec) {
-          ec = ec.previousSibling;
-          eo = ec.nodeType === 3 ? ec.nodeValue.length : ec.childNodes.length;
-        } else {
-          debug('could not find Node within %o, resetting `ec`', range.endContainer);
-          if (collapsed) {
-            ec = sc;
-            eo = so;
-          } else {
-            ec = range.endContainer;
-            eo = range.endOffset;
-          }
-          break;
-        }
-      }
-
-
-      if (ec.nodeType === 1) {
-        if (voidElements[ec.nodeName]) {
-          debug('end is a "void element", need to use parent node', ec);
-          var v = ec;
-          ec = v.parentNode;
-          eo = indexOf(ec.childNodes, v) + 1;
-          break;
-        } else {
-          debug('end is an Element, need to find deepest node at offset %o', eo);
-          end = true;
-
-          if (eo >= ec.childNodes.length) {
-            ec = ec.childNodes[ec.childNodes.length - 1];
-          } else {
-            ec = ec.childNodes[eo - 1];
-          }
-
-          var c;
-          while (ec) {
-            if (ec.nodeType === 3) break;
-            c = ec.lastChild;
-            if (c && voidElements[c.nodeName]) break;
-            ec = c;
-          }
-
-          if (ec) {
-            if (ec.nodeType === 1) {
-              // a "void element"'s parent
-              eo = ec.childNodes.length;
-            } else {
-              // text node
-              eo = ec.nodeValue.length;
-            }
-            break;
-          } else {
-            debug('could not find TextNode within %o, resetting `ec`', range.endContainer);
-            if (collapsed) {
-              ec = sc;
-              eo = so;
-            } else {
-              ec = range.endContainer;
-              eo = range.endOffset;
-            }
-            break;
-          }
-        }
-      } else {
-        break;
-      }
-    }
+  // Define a predicate to check if a Node is a leaf Node inside the Range.
+  function isLeafNodeInRange (node) {
+    if (node.childNodes.length) return false;
+    var length = node.length || 0;
+    if (node === sc && so === length) return false;
+    if (node === ec && eo === 0) return false;
+    return true;
   }
 
+  // Move the start container until it is included or collapses to the end.
+  while (start && !isLeafNodeInRange(start) && start !== end) {
+    start = documentForward(start);
+  }
 
-  if (sc !== range.startContainer || so !== range.startOffset) {
-    debug('normalizing Range `start` to %o %o:', sc, so);
+  if (start === sc) {
     range.setStart(sc, so);
+  } else if (start.nodeType === 3) {
+    range.setStart(start, 0);
+  } else {
+    range.setStartBefore(start);
   }
-  if (ec !== range.endContainer || eo !== range.endOffset) {
-    debug('normalizing Range `end` to %o %o:', ec, eo);
+
+  // Move the end container until it is included or collapses to the start.
+  while (end && !isLeafNodeInRange(end) && end !== start) {
+    end = documentReverse(end);
+  }
+
+  if (end === ec) {
     range.setEnd(ec, eo);
+  } else if (end.nodeType === 3) {
+    range.setEnd(end, end.length);
+  } else {
+    range.setEndAfter(end);
   }
 
   return range;
+}
+
+
+/**
+ * Return the next Node in a document order traversal.
+ * This order is equivalent to a classic pre-order.
+ */
+function documentForward (node) {
+  if (node.firstChild) return node.firstChild;
+
+  while (!node.nextSibling) {
+    node = node.parentNode;
+    if (!node) return null;
+  }
+
+  return node.nextSibling;
+}
+
+
+/**
+ * Return the next Node in a reverse document order traversal.
+ * This order is equivalent to pre-order with the child order reversed.
+ */
+function documentReverse (node) {
+  if (node.lastChild) return node.lastChild;
+
+  while (!node.previousSibling) {
+    node = node.parentNode;
+    if (!node) return null;
+  }
+
+  return node.previousSibling;
+}
+
+
+/* Find the first leaf node. */
+function firstLeaf (node) {
+  while (node.firstChild) node = node.firstChild;
+  return node
+}
+
+
+/* Find the last leaf node. */
+function lastLeaf (node) {
+  while (node.lastChild) node = node.lastChild;
+  return node;
 }
